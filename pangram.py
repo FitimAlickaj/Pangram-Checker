9a624fea9efb79aa1fcf1a1e8ac678302970fbdd
Fitim Alickaj <72478387+FitimAlickaj@users.noreply.github.com>
2022-05-08 00:14:39 +0200

    Update pangram.py

--- pangram.py
+++ pangram.py
@@ -1,1 +1,13 @@
-
+userI = int(input())
+userdata = str(input())
+a = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+userdata = userdata.lower()
+for i in userdata:
+    if i in a:
+        a.remove(i)
+        if len(a) == 0:
+            break
+if (len(a) == 0):
+    print("YES")
+else:
+    print("NO")
